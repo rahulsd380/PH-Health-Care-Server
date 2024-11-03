59bcfca6b865502911b537050c4b73eb60417cff
Rahul Sutradhar <rahulsd380@gmail.com>
2024-11-03 22:02:20 +0600

    Added search filter on specific field

--- src/app/modules/admin/admin.service.ts
+++ src/app/modules/admin/admin.service.ts
@@ -1,25 +1,56 @@
-import { PrismaClient } from "@prisma/client";
+import { Prisma, PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient
 
 const getAllAdmins = async (params: any) => {
-    const result = await prisma.admin.findMany({
-        where: {
-            OR: [
-                {
-                    name: {
+    const { searchTerm, ...filterData } = params;
+    const andConditions: Prisma.AdminWhereInput[] = [];
+
+
+    // [
+    //     {
+    //         name: {
+    //             contains: params.searchTerm,
+    //             mode: 'insensitive'
+    //         }
+    //     },
+    //     {
+    //         email: {
+    //             contains: params.searchTerm,
+    //             mode: 'insensitive'
+    //         }
+    //     }
+    // ]
+
+    const adminSearchableFields = ['name', 'email'];
+
+
+    if (params.searchTerm) {
+        andConditions.push(
+            {
+                OR: adminSearchableFields.map(field => ({
+                    [field]: {
                         contains: params.searchTerm,
                         mode: 'insensitive'
                     }
-                },
-                {
-                    email: {
-                        contains: params.searchTerm,
-                        mode: 'insensitive'
-                    }
+                }))
+            }
+        )
+    }
+
+    if (Object.keys(filterData).length > 0) {
+        andConditions.push({
+            AND: Object.keys(filterData).map(key => ({
+                [key]: {
+                    equals: filterData[key]
                 }
-            ]
-        }
+            }))
+        })
+    }
+
+    const whereConditions: Prisma.AdminWhereInput = { AND: andConditions }
+    const result = await prisma.admin.findMany({
+        where: whereConditions
     });
     return result;
 
